dbccf3710ea50071b2af9e060306dc5c71d9da8a
GuceAlkaTruse <60811855+GuceAlkaTruse@users.noreply.github.com>
2020-03-27 16:32:20 +0200

    first commit

---
+++ Enemy Generation/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPoints = new Transform[4];
+    [SerializeField] private GameObject _enemy;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        for (int i = 0; i < _spawnPoints.Length; i++)
+        {
+            int RandomSpawn = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_enemy, _spawnPoints[RandomSpawn]);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
